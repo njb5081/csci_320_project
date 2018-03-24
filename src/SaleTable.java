3d1c58c94e4ddac14816384d340208b44867f649
cjb5326 <cjb5326@cs.rit.edu>
2018-03-24 15:51:40 -0400

    Stubbed functions for SaleTable.java and VehicleSoldTable.java Added SQL for creating the sale and vehicle_sold table

--- src/SaleTable.java
+++ src/SaleTable.java
@@ -1,5 +1,47 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by Nicholas on 3/19/2018.
+ * Modified by Chris Baudouin
  */
 public class SaleTable {
+
+    public static void createSaleTable(Connection conn){
+        try {
+            String query = "CREATE TABLE IF NOT EXISTS sale("
+                    + "SALE_ID INT PRIMARY KEY,"
+                    + "SALEPERSON_ID INT FOREIGN KEY,"
+                    + "DEALER_ID INT FOREIGN KEY,"
+                    + "TOTAL INT,"
+                    + "DATE VARCHAR(10),"
+                    + ");" ;
+
+            /**
+             * Create a query and execute
+             */
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void importFromCSV(String file){
+
+    }
+
+    public String getSale(int sale_id, int salesperson_id, int customer_id, int dealer_id){
+        return "";
+    }
+
+    public int insertSale(int sale_id, int salesperson_id, int customer_id, int dealer_id){
+        return 0;
+    }
+
+    public int updateSale(int sale_id, int salesperson_id, int customer_id, int dealer_id){
+        return 0;
+    }
+
 }
--- src/VehicleSoldTable.java
+++ src/VehicleSoldTable.java
@@ -1,5 +1,43 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by Nicholas on 3/19/2018.
+ * Modified by Chris Baudouin
  */
 public class VehicleSoldTable {
+    public static void createVehicleSoldTable(Connection conn){
+        try {
+            String query = "CREATE TABLE IF NOT EXISTS vehicle_sold("
+                    + "SALE_ID INT PRIMARY KEY,"
+                    + "VIN VARCHAR(17) ,"
+                    + ");" ;
+
+            /**
+             * Create a query and execute
+             */
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void importFromCSV(String file){
+
+    }
+
+    public String getVehicleSoldRecord(int sale_id){
+        return "";
+    }
+
+    public int insertVehicleSoldRecord(String vin){
+        return 0;
+    }
+
+    public int updateVehicleSoldRecord(int sale_id){
+        return 0;
+    }
+
 }
